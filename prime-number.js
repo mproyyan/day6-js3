06fbc38ce2299908d4299caf297c58b7b12b2ff4
mproyyan <mproyyan@gmail.com>
2024-07-29 13:38:10 +0700

    [Feature] create function to identify prime number

---
+++ prime-number.js
@@ -0,0 +1,17 @@
+/**
+ *
+ * Write a Node.js function isPrime(n) that takes an integer n as an argument and returns true if n is a prime number and false otherwise.
+ *
+ */
+function isPrime(n) {
+    for (let i = 2; i < n; i++) {
+        if (n % i == 0) {
+            return false;
+        }
+    }
+
+    return true
+}
+
+console.log(isPrime(10));
+console.log(isPrime(43));
